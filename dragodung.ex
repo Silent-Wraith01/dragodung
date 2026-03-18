d112c34373ef9f86209a526ca0e015572931a3ae
Silent-Wraith01 <gamer20@live.com>
2026-03-18 15:59:37 -0500

    Add files via upload
    The first stages of an epic text based game

---
+++ dragodung.ex
@@ -0,0 +1,18 @@
+defmodule Dragodung do
+  @moduledoc """
+  Documentation for `Dragodung`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dragodung.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
